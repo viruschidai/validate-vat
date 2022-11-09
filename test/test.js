722e8c3b5a02db109ed93eec917e04e83c10a656
Ben Bucksch <ben.bucksch@beonex.com>
2022-11-09 05:30:57 +0100

    Tests: Show server validated in test results

--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 const validateVAT = require("../src/index");
 
-(async () => {
+(async () => await test())();
+
+async function test() {
+  await testVAT("DE183362587");
+  await testVAT("FR60421938861");
+}
+
+async function testVAT(vatID) {
   try {
-    var vatID = "DE183362587";
     //var vatID = "FR60421938861";
     var result = await validateVAT(vatID);
-    console.log(result.valid ? "valid" : "not valid");
+    console.log(result.serverValidated ? "EU server says" : "Not validated by server, but presumed", result.valid ? "valid" : "not valid");
     console.log(result);
   } catch (ex) {
     console.error(ex);
   }
-})();
+}
